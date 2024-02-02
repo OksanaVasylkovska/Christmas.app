af59a02a6222762641e0ddc15a062894dd65a3c1
OksanaVasylkovska <139795295+OksanaVasylkovska@users.noreply.github.com>
2024-02-02 23:10:53 +0200

    Update App.js
     change App

--- src/App.js
+++ src/App.js
@@ -1,71 +1,36 @@
-import './App.css';
+import "./App.css";
 
 import React from "react";
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Home from './Home';
-import AdventCalendar from './AdventCalendar';
-import logo from './assets/logo/logo.png';
-import ChristmasMovie from './ChristmasMovie';
-import Footer from './Footer';
-import { useLayoutEffect } from 'react';
-import gsap from 'gsap';
+import Home from "./Home";
+import AdventCalendar from "./AdventCalendar";
+import ChristmasMovie from "./ChristmasMovie";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
 
 
 function App() {
-
-  useLayoutEffect(() => {
-    gsap.to(".Logo-container", { rotation: 360, duration: 3 });
-    gsap.to(".Home-coverText-container", { y: 20, opacity: 1, ease: "bounce", duration: 3, delay: 3 });
-    gsap.to(".HomeWaiting-icon-container", {
-      rotation: 360,
-      duration: 3,
-      delay: 4,
-    });
-    gsap.to(".calendar-day", {
-       opacity: 1,
-       ease: "power2.inOut",
-       duration: 4,
-     });
-  }, []);
+  
 
   return (
-    <Router> 
-      <div className="Top-container" >
-        <nav className="Navbar">
-          <Link to="/" className="Link">
-            <div className="Logo-container">
-              <img className="Logo-icon" src={logo} alt="logo" />
-            </div>
-          </Link>
-          <Link to="/calendar" className="Link">
-            Advent calendar
-          </Link>
-          <Link to="/movies" className="Link">
-            Christmas movies
-          </Link>
-        </nav>
+    <Router>
+      <div className="Top-container">
+        <Navbar />
       </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/calendar" element={<AdventCalendar />} />
-        <Route path="/movies" element={<ChristmasMovie /> }/>
+        <Route path="/movies" element={<ChristmasMovie />} />
       </Routes>
 
       <footer>
-       <Footer />
-    </footer>
-    
+        <Footer />
+      </footer>
     </Router>
-
-  
   );
 }
 
 export default App;
+
